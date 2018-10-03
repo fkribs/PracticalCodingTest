8848f630c88121c1414fe8c1e71b41070badc597
fkribs <ffkribs@gmail.com>
2018-10-03 10:56:26 -0600

    first commit

---
+++ Adapters/UserAdapter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using PracticalCodingTest.Models;
+namespace PracticalCodingTest.Adapters
+{
+    class UserAdapter : BaseAdapter<User>
+    {
+        List<User> users;
+        Activity context;
+        public UserAdapter(Activity context, List<User> users) : base()
+        {
+            this.context = context;
+            this.users = users;
+
+        }
+        public override User this[int position]
+        {
+            get { return users[position]; }
+        }
+
+        public override int Count => users.Count;
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var user = users[position];
+            View view = convertView;
+            if (view == null)
+            {
+                view = context.LayoutInflater.Inflate(Resource.Layout.list_item, null);
+            }
+            view.FindViewById<TextView>(Resource.Id.textViewName).Text = user.UserName;
+            //view.FindViewById<TextView>(Resource.Id.textViewName).Text = user.UserName;
+            Random rnd = new Random();
+            Android.Graphics.Color color = Android.Graphics.Color.Rgb(rnd.Next(256),rnd.Next(256), rnd.Next(256));
+            view.FindViewById<ImageView>(Resource.Id.imageView1).SetBackgroundColor(color);
+
+            return view;
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.Views;
+using Android.OS;
+using System.Collections.Generic;
+using PracticalCodingTest.Models;
+using PracticalCodingTest.Adapters;
+
+namespace PracticalCodingTest
+{
+    [Activity(Label = "PracticalCodingTest", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        List<User> users = new List<User>();
+        ListView listView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            listView = FindViewById<ListView>(Resource.Id.lvContacts);
+
+            for (int i = 1; i < 20; i++)
+            {
+
+                users.Add(new User
+                {
+                    UserName = "Forrest",
+                    PasswordHash = "90"
+                });
+            }
+
+            listView.Adapter = new UserAdapter(this, users);
+        }
+    }
+}
+
